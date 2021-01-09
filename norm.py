e479e42e2629f11fd2dd5a00f1442fa4e428e3f4
Karol Guzikowski <karolguzikowski@gmail.com>
2021-01-09 17:20:29 +0100

    learning code done, data is splitted into test and train

--- norm.py
+++ norm.py
@@ -1,5 +1,7 @@
 import csv, pandas as pd
+
 df = pd.read_csv('DoIHaveAChance_ML_project/speed_dating.csv')
+
 def func(lista):
     selected_df = df[lista]
     print(selected_df)
@@ -14,8 +16,10 @@ lista = ["attr1_1", "sinc1_1", "intel1_1", "fun1_1", "amb1_1", "shar1_1"]
 lista1 = ["attr_o", "sinc_o", "intel_o", "fun_o", "amb_o", "shar_o"]
 lista3 = ["attr2_1", "sinc2_1", "intel2_1", "fun2_1", "amb2_1", "shar2_1"]
 lista4 = ["attr1_2", "sinc1_2", "intel1_2", "fun1_2", "amb1_2", "shar1_2"]
+
 func(lista)
 func(lista1)
 func(lista3)
 func(lista4)
+
 df.to_csv('filtered_data.csv', index=False)
